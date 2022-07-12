9ae7c51aeaff766cf8f0cc84994866688d295635
Giovanne Mendonca <giovannemendonca@outlook.com>
2022-07-12 18:08:07 -0300

    Atualização

--- main.js
+++ main.js
@@ -1,31 +1,20 @@
-// const cartFruits = ['Banana', 'Uva', 'Laraja', 'Kiwi', 'Abacate', 'Mamão'];
-
-// const list = document.querySelector('.list');
-
-
-// cartFruits.forEach(element => {
-//     let item = element;
-//     let newItem = document.createElement('li')
-    
-//     newItem.classList.add('item');
-//     newItem.innerHTML = element;
-
-//     list.appendChild(newItem);
-
-// })
-
-
-
 const list = document.querySelector('.list');
-
 const newItem = document.querySelector('.input-text');
 const form = document.querySelector('.form')
 
 
 form.addEventListener('submit', (event) => {
-    event.preventDefault()
 
-    console.log(event.target.elements['nome'].value)
+    event.preventDefault();
+    addElements(event.target.elements['nome'].value);
+
 })
 
+function addElements(elemento){
+    let li = document.createElement('li');
+    li.classList.add('item');
+    li.innerHTML = elemento;
+
+    list.appendChild(li);
+}
 
